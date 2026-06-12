569c6f83455ed44f20a086f89723ac8c2b947caf
omeroastore-ops <omero.astore@gmail.com>
2026-06-13 01:33:51 +0200

    Delete dist directory

--- dist/server/assets/holzkohle-new-DmR6SYyx.js
+++
@@ -1,4 +0,0 @@
-//#region src/assets/holzkohle-new.jpg
-var holzkohle_new_default = "/charcoal/assets/holzkohle-new-D7wcprgo.jpg";
-//#endregion
-export { holzkohle_new_default as t };
--- dist/server/assets/meiler-feuer-new-CJ7tTcvT.js
+++
@@ -1,4 +0,0 @@
-//#region src/assets/meiler-feuer-new.jpg
-var meiler_feuer_new_default = "/charcoal/assets/meiler-feuer-new-C3yntkh9.jpg";
-//#endregion
-export { meiler_feuer_new_default as t };
